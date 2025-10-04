6edcd7174b71d3d53f54f5a2af02b8503a1dc485
dnh_an <dinhan.0321@gmail.com>
2025-10-04 18:10:02 +0700

    UPDATE: adapt laravel kit -> lafins

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -27,7 +27,7 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
+        href: 'https://github.com/dnhfan/lafins.git',
         icon: Folder,
     },
     {
