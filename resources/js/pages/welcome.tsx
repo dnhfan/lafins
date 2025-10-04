e3575121c8a3e010762b12cf43f7a0fbc25c33b6
dnh_an <dinhan.0321@gmail.com>
2025-10-04 19:06:25 +0700

    UPADATE: change a litle bit in wellcome

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -49,7 +49,7 @@ export default function Welcome() {
                                 Let's get started
                             </h1>
                             <p className="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
-                                Laravel has an incredibly rich ecosystem.
+                                Lafins is the best Financial managerment ur can never found.
                                 <br />
                                 We suggest starting with the following.
                             </p>
