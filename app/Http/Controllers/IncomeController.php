bf14a5aafe388d02dbd2715abc6d56f3bc9cd23c
dnh_an <dinhan.0321@gmail.com>
2025-10-15 11:22:23 +0700

    FIX: err when using middleware method in IncomeController

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -9,6 +9,11 @@ use Inertia\Inertia;
 
 class IncomeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      */
