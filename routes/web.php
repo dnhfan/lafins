45dab9c3259e677bcbc6bd633728afb78b24e831
dnh_an <dinhan.0321@gmail.com>
2025-10-22 16:14:42 +0700

    upate: route for income store

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('incomes', [IncomeController::class, 'index'])->name('incomes');
-    
+    Route::post('incomes', [IncomeController::class, 'store'])->name('incomes.store');
 });
 
 require __DIR__.'/settings.php';
