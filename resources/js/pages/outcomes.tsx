e46dfa7d6376d4043f6e43881d5d11d1f617cdbe
dnh_an <dinhan.0321@gmail.com>
2025-10-23 01:05:50 +0700

    ADD: outcome views

--- resources/js/pages/outcomes.tsx
+++ resources/js/pages/outcomes.tsx
@@ -1,6 +1,9 @@
+import FSbox from "@/components/FSbox";
 import AppLayout from "@/layouts/app-layout";
 import { BreadcrumbItem } from "@/types";
 import { Head } from "@inertiajs/react";
+import AddOutcomeModal from "./outcomes/AddOutcomeModal";
+import OutcomeTable from "./outcomes/OutcomeTable";
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -17,7 +20,18 @@ export default function Outcomes() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Outcomes" />
             <main className="flex flex-1 flex-col gap-4 overflow-auto rounded-xl p-4">
-                
+                <FSbox 
+                    endpoint="/outcomes"
+                    addTitle="Add new outcome"
+                    AddModalComponent={AddOutcomeModal}
+                    sortFields={[
+                        { label: 'Date', value: 'date' },
+                        { label: 'Amount', value: 'amount' }
+                    ]}
+                    defaultSortBy="date"
+                    defaultSortDir="desc"
+            />
+                <OutcomeTable />
             </main>
         </AppLayout>
     )
