7e879cfb690308c80099eee0e903873b120762e0
dnh_an <dinhan.0321@gmail.com>
2025-10-22 20:16:02 +0700

    ADD: success alert

---
+++ resources/js/components/success-dialog.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+
+type Props = {
+  message?: string | null;
+  title?: string;
+  confirmText?: string;
+  onClose?: () => void;
+};
+
+// SuccessDialog: show a modal when message is provided, allows user to confirm/close.
+export default function SuccessDialog({
+  message,
+  title = 'Thành công',
+  confirmText = 'Xác nhận',
+  onClose,
+}: Props) {
+  const [open, setOpen] = useState<boolean>(Boolean(message));
+
+  useEffect(() => {
+    setOpen(Boolean(message));
+  }, [message]);
+
+  const handleClose = () => {
+    setOpen(false);
+    onClose?.();
+  };
+
+  if (!message) return null;
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{message}</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button onClick={handleClose}>{confirmText}</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
