2cd522d5bb6bccd14c9cb262ccdddcf0a6c797e1
dnh_an <dinhan.0321@gmail.com>
2025-10-23 11:07:59 +0700

    UPDATE: route for jars controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use \App\Http\Controllers\IncomeController;
+use App\Http\Controllers\JarsController;
 use App\Http\Controllers\OutcomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,6 +24,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('outcomes', [OutcomeController::class, 'store'])->name('outcomes.store');
     Route::put('outcomes/{outcome}', [OutcomeController::class, 'update'])->name('outcomes.update');
     Route::delete('outcomes/{outcome}', [OutcomeController::class, 'destroy'])->name('outcomes.destroy');
+
+    Route::get('jarconfigs', [JarsController::class, 'index'])->name('jarconfigs');
+    // Jar config actions
+    Route::put('jarconfigs/{jar}', [JarsController::class, 'update'])->name('jarconfigs.update');
+    Route::post('jarconfigs/bulk-update', [JarsController::class, 'bulkUpdate'])->name('jarconfigs.bulkUpdate');
+    Route::post('jarconfigs/reset', [JarsController::class, 'reset'])->name('jarconfigs.reset');
+    Route::post('jarconfigs/delete-all', [JarsController::class, 'deleteAll'])->name('jarconfigs.deleteAll');
 });
 
 require __DIR__.'/settings.php';
