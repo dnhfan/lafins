fa21dd5c2edc4af96b8b0677486808c64c467c78
dnhfan <dinhan.0321@gmail.com>
2026-01-07 23:55:48 +0700

    fix: handle exception in login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
+use Exception;
 
 class AuthController extends Controller
 {
@@ -39,15 +41,27 @@ class AuthController extends Controller
 
     public function login(LoginRequest $loginRequest)
     {
-        $loginRequest->authenticate();
+        try {
+            $user = $loginRequest->authenticate();
 
-        $user = $loginRequest->user();
-        $token = $user->createToken('auth-token')->plainTextToken;
+            $token = $user->createToken('auth-token')->plainTextToken;
 
-        return response()->json([
-            'user' => $user,
-            'token' => $token,
-        ]);
+            return response()->json([
+                'user' => $user,
+                'token' => $token,
+            ], 200);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Wrong password or username.',
+                'error' => $e->getMessage()
+            ], 422);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'LOGIN ERROR: ' . $e->getMessage()
+            ], 500);
+        }
     }
 
     public function logout(Request $request)
