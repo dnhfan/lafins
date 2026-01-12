d6819e386180666d062a6406fcf474ffa2d769b3
dnhfan <dinhan.0321@gmail.com>
2026-01-12 11:53:39 +0700

    feat(back): complete api endpoint, add rate limiter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use App\Models\User;
 use App\Observers\UserObserver;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     {
         // Register model observers
         User::observe(UserObserver::class);
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute('60')->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\IncomeController;
 use App\Http\Controllers\Api\JarsController;
 use App\Http\Controllers\Api\OutcomeController;
+use App\Http\Controllers\Api\PasswordController;
+use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\TwoFactorAuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 // -- public routes --
@@ -13,7 +17,7 @@ Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 
 // -- protected routes --
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
     // auth & user info
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
@@ -31,4 +35,24 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
     // outcomes
     Route::apiResource('outcomes', OutcomeController::class);
+
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+
+    // Password
+    Route::put('/settings/password', [PasswordController::class, 'update']);
+
+    // 2FA
+    Route::prefix('settings/two-factor')->group(function () {
+        Route::get('/', [TwoFactorAuthenticationController::class, 'show']);
+    });
+
+    Route::controller(ProfileController::class)->prefix('profile')->group(function () {
+        // GET
+        Route::get('/', 'show')->name('profile.show');
+        // PATCH
+        Route::patch('/', 'update')->name('profile.update');
+        // DELETE
+        Route::delete('/', 'destroy')->name('profile.destroy');
+    });
 });
