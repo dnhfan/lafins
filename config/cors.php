1e2f851d1b1c5131f86d050f3276d6120749bfcd
dnhfan <dinhan.0321@gmail.com>
2026-01-20 22:43:32 +0700

    feat: cors config
    add 127.0.0.1 + localhost
    s

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,13 @@ return [
      */
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'http://localhost:8000'],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:8000',
+        'http://127.0.0.1:8000',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
