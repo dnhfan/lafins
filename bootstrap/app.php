ef2c178b6f9b2c9cee9bc8fa300f4727380771ee
dnhfan <dinhan.0321@gmail.com>
2026-01-31 10:41:47 +0700

    fix: using abilities midleware in route/api but not define it

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Laravel\Sanctum\Http\Middleware\CheckAbilities;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,22 +17,34 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'abilities' => CheckAbilities::class,
+            'ability' => CheckForAnyAbility::class,
+        ]);
+        // --------------------------------------------------------------
+        // 1. API CONFIGURATION (STATELESS)
+        // ----------------------------------------------------------------
+
         // Make API routes stateful for SPA authentication
-        $middleware->statefulApi();
-        
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
+        /* $middleware->statefulApi(); */
 
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        /* $middleware->api(prepend: [ */
+        /* \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, */
+        /* ]); */
 
-        $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        // ----------------------------------------------------------------
+        // 2. WEB CONFIGURATION (Cleanup)
+        // ----------------------------------------------------------------
 
-        $middleware->web(append: [
-            HandleAppearance::class,
-            HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
-        ]);
+        /* $middleware->append(\Illuminate\Http\Middleware\HandleCors::class); */
+        /**/
+        /* $middleware->encryptCookies(except: ['appearance', 'sidebar_state']); */
+        /**/
+        /* $middleware->web(append: [ */
+        /* HandleAppearance::class, */
+        /* HandleInertiaRequests::class, */
+        /* AddLinkHeadersForPreloadedAssets::class, */
+        /* ]); */
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
